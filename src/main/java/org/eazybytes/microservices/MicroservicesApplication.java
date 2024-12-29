bc8e12ca3f2aa6f2f6904a64ae58b18ddc378e91
sandesh ghoti <sandesh.ghoti@gmail.com>
2024-12-29 23:50:13 +0530

    initialize app

---
+++ src/main/java/org/eazybytes/microservices/MicroservicesApplication.java
@@ -0,0 +1,13 @@
+package org.eazybytes.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesApplication.class, args);
+    }
+
+}
